f9b63b8f247784226a13f0fdb7571370a82848ef
carlos horacio lopez <krloss@gmail.com>
2022-10-20 04:12:03 -0300

    bufix: envio de formulario

--- components/navbar.js
+++ components/navbar.js
@@ -3,12 +3,12 @@ function Navbar() {
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
         <a class="navbar-brand">Navbar</a>
-        <form class="d-flex">
+        <div class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" id="searchKey" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit" onclick="filterSearch(document.getElementById('searchKey').value)">
+            <button class="btn btn-outline-success" type="button" onclick="filterSearch(document.getElementById('searchKey').value)">
                 Search
             </button>
-        </form>
+        </div>
         </div>
     </nav>
     `;
